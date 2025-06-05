56e0737fdf0e35ef0e6d1e3c05bc56df2385c540
jesusvaros <xjesusvr@gmail.com>
2025-06-05 14:53:18 -0700

    feat: add Vercel Analytics integration and update sitemap timestamps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import { DefaultSeo } from 'next-seo';
 import SEO from '../next-seo.config';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
