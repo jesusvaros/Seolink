8cb5330825e83dd5750f39c98ea4565aca1ba913
jesusvaros <xjesusvr@gmail.com>
2025-06-23 12:20:57 +0200

    feat: add shipping destination to product schema for Spain

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -321,6 +321,10 @@ export default function PostPage({ source, frontMatter }: PostProps) {
                           "value": "0",
                           "currency": "EUR"
                         },
+                        "shippingDestination": {
+                          "@type": "DefinedRegion",
+                          "addressCountry": "ES"
+                        },
                         "deliveryTime": {
                           "@type": "ShippingDeliveryTime",
                           "handlingTime": {
